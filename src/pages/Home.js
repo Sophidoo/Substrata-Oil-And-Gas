b3342005c5ac67fd96ebe6471ad795bf3c1aafdb
Sophia Okosodo <sophieokosodo@gmail.com>
2023-11-16 18:27:17 +0100

    opacity

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,13 @@ import { useEffect, useRef, useState } from "react";
 
 const Home = () => {
     const navigate = useNavigate()
-    const [ref, inView] = useInView();
+    const [ref, inView] = useInView({
+        threshold: 0.1,
+        rootMargin: '0px'
+    });
+    useEffect(() => {
+        console.log(inView)
+    }, [inView])
 
       
     
@@ -43,16 +49,16 @@ const Home = () => {
 
         <motion.div
             ref={ref}
-            animate={inView ? {opacity: 1, x: [-400, 0], y:[100, 0]} : {opacity: 0}}
+            animate={inView ? {opacity: 1} : {opacity: 0}}
             initial={{opacity: 0}}
             transition={{duration: 2, ease: "easeIn"}}
         >
-        <section className="history">
-            <h2>Substrata's History</h2>
-            <h3>Substrata Oil and Gas Limited was incorporated in 2006 to carry out oil field services to the Oil and Gas industry in Nigeria.</h3>
-            <p>Substrata Oil and Gas Limited (RC 663293) was incorporated in 2006 to carry out oil field services to the Oil and Gas industry in Nigeria. <br/> The need to reduce costs by introducing new, effective and efficient technology, coupled with the recent introduction of new Government policies aimed at encouraging and ensuring more participation by competent Nigerians in the upstream Oil & Gas Industry has played an important role in the development of Substrata Oil and Gas Limited. <br/> Substrata Oil and Gas Limited is wholly Nigerian owned with local and international affiliations and partnerships. Our operations are knowledge driven backed by technology.</p>
-            <button>Learn More <FaArrowRight /></button>
-        </section>
+            <section className="history">
+                <h2>Substrata's History</h2>
+                <h3>Substrata Oil and Gas Limited was incorporated in 2006 to carry out oil field services to the Oil and Gas industry in Nigeria.</h3>
+                <p>Substrata Oil and Gas Limited (RC 663293) was incorporated in 2006 to carry out oil field services to the Oil and Gas industry in Nigeria. <br/> The need to reduce costs by introducing new, effective and efficient technology, coupled with the recent introduction of new Government policies aimed at encouraging and ensuring more participation by competent Nigerians in the upstream Oil & Gas Industry has played an important role in the development of Substrata Oil and Gas Limited. <br/> Substrata Oil and Gas Limited is wholly Nigerian owned with local and international affiliations and partnerships. Our operations are knowledge driven backed by technology.</p>
+                <button>Learn More <FaArrowRight /></button>
+            </section>
         </motion.div>
 
     </>
