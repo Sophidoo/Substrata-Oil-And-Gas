8625455dddf8abd180560fb6d8412ae554ee0100
Sophia Okosodo <sophieokosodo@gmail.com>
2023-11-20 18:21:14 +0100

    mission and vision area

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,12 @@ import Navbar from "../components/Navbar"
 import "../styles/About.css"
 import Footer from "../components/Footer";
 import { FaHandshake } from "react-icons/fa6";
+import { IoIosPerson } from "react-icons/io";
+import { HiUserGroup } from "react-icons/hi2";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { GiCargoCrane } from "react-icons/gi";
+import mission from "../assets/images/mission.jpg"
+import vission from "../assets/images/vission.jpg"
 
 const About = () => {
     
@@ -26,7 +32,7 @@ const About = () => {
         <div className="missionVisionWrapper">
             <div className="missionWrapper">
                 <div className="mission">
-                    <h3>our Mission</h3>
+                    <h3>Our Mission</h3>
                     <p>
                     To provide the best value in services for the oil and gas industry in Nigeria and Africa. Best value means providing the fit-for-purpose solutions that best meet our client’s criteria and resources through the following:
                     <ul>
@@ -36,14 +42,14 @@ const About = () => {
                     </ul>
                     </p>
                 </div>
-                <img src="" alt="" />
+                <img src={mission} alt="" />
             </div>
-            <div className="missionWrapper">
+            <div className="missionWrapper vissionWrapper">
                 <div className="mission vision">
                     <h3>Our Vision</h3>
                     <p>To become the point of reference in the Africa Oil and Gas industry, known for world class service delivery with utmost professionalism.</p>
                 </div>
-                <img src="" alt="" />
+                <img src={vission} alt="" />
             </div>
         </div>
 
@@ -56,18 +62,22 @@ const About = () => {
                     <p>At our core, integrity defines us. We are dedicated to upholding unwavering honesty and ethical principles, ensuring trust and delivering excellence in all aspects of our operations, cultivating enduring and meaningful partnerships.</p>
                 </div>
                 <div className="valuesCard">
+                <IoIosPerson/>
                     <h3>Personnel</h3>
                     <p>Our skilled personnel are the backbone of our success. They are dedicated, knowledgeable, and committed to ensuring top-notch performance and delivering quality service to our clients.</p>
                 </div>
                 <div className="valuesCard">
+                <RiCustomerService2Fill/>
                     <h3>Service</h3>
                     <p>Our commitment to service excellence is unwavering. We strive to provide unparalleled solutions, consistently exceeding expectations, and ensuring our clients receive the utmost satisfaction, building trust and enduring relationships.</p>
                 </div>
                 <div className="valuesCard">
+                <GiCargoCrane/>
                     <h3>Technology</h3>
                     <p>Leveraging cutting-edge technology is at the heart of our operations. We continuously embrace innovation to enhance efficiency, accuracy, and competitiveness in delivering solutions that meet industry demands.</p>
                 </div>
                 <div className="valuesCard">
+                <HiUserGroup/>
                     <h3>Social Responsibility</h3>
                     <p>Social responsibility is integral to our ethos. We are deeply committed to giving back to the communities we operate in, promoting sustainability, and fostering a positive impact on society and the environment.</p>
                 </div>
